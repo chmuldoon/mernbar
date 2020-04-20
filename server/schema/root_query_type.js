20af9f5a2010b024de5b742426e62f482ba4abcc
Charles Muldoon <charm3728@gmail.com>
2020-04-20 02:29:48 -0700

    established graphql tree

--- server/schema/root_query_type.js
+++ server/schema/root_query_type.js
@@ -2,17 +2,32 @@ const mongoose = require("mongoose");
 const graphql = require("graphql");
 const { GraphQLObjectType, GraphQLList, GraphQLID, GraphQLNonNull } = graphql;
 const IngredientType = require("./ingredient_type")
-const Ingredient = mongoose.model("ingredient")
+const Ingredient = require('../models/Ingredient')
+const CocktailType = require("./cocktail_type");
+const Cocktail = require('../models/cocktail')
 
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: () => ({
-    ingredients:{
+    ingredients: {
       type: new GraphQLList(IngredientType),
       resolve() {
-        return Ingredient.find({})
-      }
-    }
+        return Ingredient.find({});
+      },
+    },
+    cocktails: {
+      type: new GraphQLList(CocktailType),
+      resolve() {
+        return Cocktail.find({});
+      },
+    },
+    ingredient: {
+      type: IngredientType,
+      args: { id: { type: new GraphQLNonNull(GraphQLID) } },
+      resolve(parentValue, { id }) {
+        return Ingredient.findById(id);
+      },
+    },
   }),
 });
 
