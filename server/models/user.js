24e98375a4bc137ee447378f7f9ae86c3ef3ffe3
Charles Muldoon <charm3728@gmail.com>
2020-04-23 17:41:39 -0700

    added user auth graphql

--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  name: {
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
     type: String,
     required: true,
     unique: true,
@@ -10,10 +15,37 @@ const UserSchema = new mongoose.Schema({
     type: [mongoose.Schema.Types.ObjectId],
     ref: "ingredient",
   },
-  cocktailss: {
+  cocktails: {
     type: [mongoose.Schema.Types.ObjectId],
     ref: "cocktails",
   },
 });
+UserSchema.pre("save", function save(next) {
+  const user = this;
+  if (!user.isModified("password")) {
+    return next();
+  }
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+    bcrypt.hash(user.password, salt, null, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+UserSchema.methods.comparePassword = function comparePassword(
+  candidatePassword,
+  cb
+) {
+  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+    cb(err, isMatch);
+  });
+};
 
 module.exports = User = mongoose.model("user", UserSchema);
--- server/schema/root_query_type.js
+++ server/schema/root_query_type.js
@@ -4,11 +4,19 @@ const { GraphQLObjectType, GraphQLList, GraphQLID, GraphQLNonNull } = graphql;
 const IngredientType = require("./ingredient_type")
 const Ingredient = require('../models/Ingredient')
 const CocktailType = require("./cocktail_type");
+const UserType = require("./user_type");
+
 const Cocktail = require('../models/cocktail')
 
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: () => ({
+    currentUser: {
+        type: UserType,
+        resolve(parentValue, arg, req){
+          return req.user;
+        }
+    },
     ingredients: {
       type: new GraphQLList(IngredientType),
       resolve() {
